4736ee071917c8b8937a92f120421dbeaf7e5998
DarrenTef <darrenlee6289@gmail.com>
2024-04-29 23:21:36 -0700

    Learning how to branch but also including scanning functionality which scans an image and sends it to processImageScreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -3,27 +3,53 @@
  * 
  * Home screen component.
  */
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { HomeScreenNavigationProps } from '../Navigator';
 import { FloatingAction } from "react-native-floating-action";
 import * as ImagePicker from 'react-native-image-picker';
 import {ImagePickerResponse} from 'react-native-image-picker/src/types';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons'
-
+import DocumentScanner from 'react-native-document-scanner-plugin'
+import { useNavigation } from '@react-navigation/native';
+import { DemoButton } from '../components/ui/DemoButton';
+import * as routes from '../routes';
 
 export default function HomeScreen(props: HomeScreenNavigationProps) {
+  const [scannedImage, setScannedImage] = useState<string | undefined>();
 
-  const [response, setResponse] = useState<ImagePickerResponse | null>(null);
+  const scanDocument = async () => {
+    // start the document scanner
+    const { scannedImages }= await DocumentScanner.scanDocument()
+    
+    // get back an array with scanned image file paths
+    if (scannedImages && scannedImages.length > 0) {
+      // set the img src, so we can view the first scanned image
+      setScannedImage(scannedImages[0])
+      onProcessImage(scannedImages[0])
+    }
+    else {
+      setScannedImage(undefined)
+    }
+  }
+
+  const onProcessImage = (uri: string | undefined) => {
+    if (uri) {
+      props.navigation.navigate(routes.PROCESS_IMAGE_SCREEN, {
+        uri: uri,
+      });
+    }
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Home</Text>
-      <FloatingAction position="center" />
+      <DemoButton key="Process Image" onPress={ () => scanDocument()}>
+          {'Process Image'}
+        </DemoButton>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -35,3 +61,4 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
 });
+

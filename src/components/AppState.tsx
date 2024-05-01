9b8a61faff939e2597bc0a6d81244c44e089464b
David Serrato <davserrato@gmail.com>
2024-05-01 10:05:35 -0700

    added app state

---
+++ src/components/AppState.tsx
@@ -0,0 +1,14 @@
+
+export interface IFriends {
+    name: string,
+    email?: string,
+    phoneNumber?: string
+}
+
+export interface IGroups {
+    
+}
+
+export default function AppState() {
+    
+}

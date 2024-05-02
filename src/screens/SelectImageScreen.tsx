e4ba086672989c6a5dc438602bfbe0bf46ea4cf9
David Serrato <davserrato@gmail.com>
2024-05-01 19:29:46 -0700

    Fixed image display. Reordered buttons

--- src/screens/SelectImageScreen.tsx
+++ src/screens/SelectImageScreen.tsx
@@ -3,8 +3,8 @@
  * 
  * Displays buttons for capturing an image or selecting one from the gallery.
  */
-import React from 'react';
-import {View, SafeAreaView, Image, StyleSheet, useWindowDimensions, ScrollView} from 'react-native';
+import React, { useMemo } from 'react';
+import {View, SafeAreaView, Image, StyleSheet, useWindowDimensions, ScrollView, Text} from 'react-native';
 import {DemoButton} from '../components/ui';
 import * as ImagePicker from 'react-native-image-picker';
 import {ImagePickerResponse} from 'react-native-image-picker/src/types';
@@ -40,14 +40,35 @@ export const SelectImageScreen = (props: SelectScreenNavigationProps) => {
     }
   };
 
+  const aspectRatio = useMemo(() => {
+    if (response?.assets && response.assets[0] && response.assets[0].width && response.assets[0].height) {
+      return response.assets[0].width / response.assets[0].height
+    }
+    return undefined;
+  }, [response]);
+
   return (
     <SafeAreaView style={{flex: 1, flexDirection: 'column'}}>
-      <View style={{flexDirection: 'row', paddingBottom: 8}}>
-        <DemoButton key="Process Image" onPress={onProcessImage}>
-          {'Process Image'}
-        </DemoButton>
-      </View>
-      <View style={{flexDirection: 'row', paddingTop: 8}}>
+      {/*<View style={{paddingHorizontal: 8}}>
+        <DemoResponse>{response}</DemoResponse>
+        </View>*/}
+      {response?.assets ?
+        <ScrollView style={{flex: 1}}>
+          {response.assets[0] &&   
+            <Image
+              resizeMode="contain"
+              resizeMethod="auto"
+              style={{width: '100%', height: undefined, aspectRatio: aspectRatio}}
+              source={{uri: response.assets[0].uri}}
+            />
+          }          
+        </ScrollView>
+        :
+        <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+          <Text>Select an image to display</Text>
+        </View>
+      }
+      <View style={{flexDirection: 'row', paddingVertical: 8}}>
         <DemoButton
           key="Take Image"
           onPress={() =>
@@ -71,30 +92,11 @@ export const SelectImageScreen = (props: SelectScreenNavigationProps) => {
           {'Select Image'}
         </DemoButton>
       </View>
-      {/*<View style={{paddingHorizontal: 8}}>
-        <DemoResponse>{response}</DemoResponse>
-        </View>*/}
-      <View style={{flex: 1}}>
-        {response?.assets &&
-          <ScrollView style={styles.scrollView}>
-            {response.assets[0] &&   
-              <Image
-                resizeMode="center"
-                resizeMethod="auto"
-                style={{width: '100%', height: height}}
-                source={{uri: response.assets[0].uri}}
-              />
-            }          
-          </ScrollView>
-        }
+      <View style={{flexDirection: 'row', paddingBottom: 8}}>
+        <DemoButton key="Process Image" onPress={onProcessImage}>
+          {'Process Image'}
+        </DemoButton>
       </View>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    flex: 1,
-    width: '100%'
-  }
-});

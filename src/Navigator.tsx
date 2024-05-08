acd856e4506f0c78b5d8082a53bb33880dddbdb8
David Serrato <davserrato@gmail.com>
2024-05-08 13:44:48 -0700

    added routes

--- src/Navigator.tsx
+++ src/Navigator.tsx
@@ -4,7 +4,6 @@
  * Contains the routes for the app. Also configures the left drawer and the title bar.
  */
 import React from 'react';
-import * as routes from './routes';
 import {SelectImageScreen} from './screens/SelectImageScreen';
 import {RouteProp} from '@react-navigation/native';
 import { createDrawerNavigator, DrawerScreenProps } from '@react-navigation/drawer';
@@ -12,40 +11,68 @@ import HomeScreen from './screens/HomeScreen';
 import SettingsScreen from './screens/SettingsScreen';
 import GroupsScreen from './screens/GroupsScreen';
 import { ProcessImageScreen } from './screens/ProcessImageScreen';
+import * as routes from './routes';
+import LoginScreen from './screens/LoginScreen';
+import useAppContext from './components/hooks/useAppContext';
+import LogoutScreen from './screens/LogoutScreen';
+import SignUpScreen from './screens/SignUpScreen';
 
 // Typescript route types required by react-navigation
 // Defines additional prop variables for each screen.
-type RootStackParamList = {
+export type RootDrawerParamList = {
   [routes.HOME_SCREEN]: undefined,
   [routes.SELECT_SCREEN]: undefined,
   [routes.PROCESS_IMAGE_SCREEN]: {
     uri: string;
   },
   [routes.GROUPS_SCREEN]: undefined,
-  [routes.FRIENDS_SCREEN]: undefined,
-  [routes.SETTINGS_SCREEN]: undefined
+  [routes.CONTACTS_SCREEN]: undefined,
+  [routes.SETTINGS_SCREEN]: undefined,
+  [routes.LOGIN_SCREEN]: undefined,
+  [routes.LOGOUT_SCREEN]: undefined,
+  [routes.SIGNUP_SCREEN]: undefined
 };
 
 // Typescript prop types for screens
 // Adds additional methods to the prop variable such as navigate()
-export type HomeScreenNavigationProps = DrawerScreenProps<RootStackParamList, typeof routes.HOME_SCREEN>;
-export type SelectScreenNavigationProps = DrawerScreenProps<RootStackParamList, typeof routes.SELECT_SCREEN>;
-export type ProcessImageNavigationProps = DrawerScreenProps<RootStackParamList, typeof routes.PROCESS_IMAGE_SCREEN>;
-export type ProcessImageRouteProps = RouteProp<RootStackParamList, typeof routes.PROCESS_IMAGE_SCREEN>;
-export type GroupsNavigationProps = DrawerScreenProps<RootStackParamList, typeof routes.GROUPS_SCREEN>;
-export type FriendsScreenNavigationProps = DrawerScreenProps<RootStackParamList, typeof routes.FRIENDS_SCREEN>;
-export type SettingsScreenNavigationProps = DrawerScreenProps<RootStackParamList, typeof routes.SETTINGS_SCREEN>;
+export type HomeScreenNavigationProps = DrawerScreenProps<RootDrawerParamList, typeof routes.HOME_SCREEN>;
+export type SelectScreenNavigationProps = DrawerScreenProps<RootDrawerParamList, typeof routes.SELECT_SCREEN>;
+export type ProcessImageNavigationProps = DrawerScreenProps<RootDrawerParamList, typeof routes.PROCESS_IMAGE_SCREEN>;
+export type ProcessImageRouteProps = RouteProp<RootDrawerParamList, typeof routes.PROCESS_IMAGE_SCREEN>;
+export type GroupsNavigationProps = DrawerScreenProps<RootDrawerParamList, typeof routes.GROUPS_SCREEN>;
+export type ContactsScreenNavigationProps = DrawerScreenProps<RootDrawerParamList, typeof routes.CONTACTS_SCREEN>;
+export type SettingsScreenNavigationProps = DrawerScreenProps<RootDrawerParamList, typeof routes.SETTINGS_SCREEN>;
+export type LoginScreenNavigationProps = DrawerScreenProps<RootDrawerParamList, typeof routes.LOGIN_SCREEN>;
+export type LogoutScreenNavigationProps = DrawerScreenProps<RootDrawerParamList, typeof routes.LOGOUT_SCREEN>;
+export type SignUpScreenNavigationProps = DrawerScreenProps<RootDrawerParamList, typeof routes.SIGNUP_SCREEN>;
 
 // Instantiates a react-navigation drawer with the current route up top
-const Drawer = createDrawerNavigator<RootStackParamList>();
+const Drawer = createDrawerNavigator<RootDrawerParamList>();
 
 // Defines our routing and the component they route to
-export const Navigator = () => (
-  <Drawer.Navigator>
-    <Drawer.Screen name={routes.HOME_SCREEN} component={HomeScreen} />
-    <Drawer.Screen name={routes.SELECT_SCREEN} component={SelectImageScreen} />
-    <Drawer.Screen name={routes.PROCESS_IMAGE_SCREEN} component={ProcessImageScreen} options={{drawerItemStyle: {display: "none"}}} />
-    <Drawer.Screen name={routes.GROUPS_SCREEN} component={GroupsScreen} />
-    <Drawer.Screen name={routes.SETTINGS_SCREEN} component={SettingsScreen} />
-  </Drawer.Navigator>
-);
+export const Navigator = () => {
+
+  const ctx = useAppContext();
+
+  return (
+    <Drawer.Navigator>
+      {ctx.user == null ? (
+          <>
+            <Drawer.Screen name={routes.LOGIN_SCREEN} component={LoginScreen} options={{drawerItemStyle: {display: "none"}}} />
+            <Drawer.Screen name={routes.SIGNUP_SCREEN} component={SignUpScreen} options={{drawerItemStyle: {display: "none"}}} />
+          </>
+        ) : 
+        (
+          <>
+            <Drawer.Screen name={routes.HOME_SCREEN} component={HomeScreen} />
+            <Drawer.Screen name={routes.SELECT_SCREEN} component={SelectImageScreen} />
+            <Drawer.Screen name={routes.PROCESS_IMAGE_SCREEN} component={ProcessImageScreen} options={{drawerItemStyle: {display: "none"}}} />
+            
+            <Drawer.Screen name={routes.LOGOUT_SCREEN} component={LogoutScreen} options={{drawerItemStyle: {display: "none"}}} />
+            <Drawer.Screen name={routes.GROUPS_SCREEN} component={GroupsScreen} />
+            <Drawer.Screen name={routes.SETTINGS_SCREEN} component={SettingsScreen} />
+          </>
+        )}
+    </Drawer.Navigator>
+  )
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,6 @@
+import { DrawerScreenProps } from "@react-navigation/drawer";
+import { RouteProp } from "@react-navigation/native";
+
 /**
  * routes.ts
  * 
@@ -5,8 +8,10 @@
  */
 export const HOME_SCREEN = "Home";
 export const SELECT_SCREEN = 'Select Image';
-export const PROCESS_IMAGE_SCREEN = 'Process image';
+export const PROCESS_IMAGE_SCREEN = 'Process Image';
 export const SETTINGS_SCREEN = "Settings";
 export const GROUPS_SCREEN = "Groups";
-export const FRIENDS_SCREEN = "Friends";
-
+export const CONTACTS_SCREEN = "Contacts";
+export const LOGIN_SCREEN = "Login";
+export const LOGOUT_SCREEN = "Logout";
+export const SIGNUP_SCREEN = "Sign Up";

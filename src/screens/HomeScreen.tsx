025c78a08bd2c3b5fe6654e819c146d49516c546
David Serrato <davserrato@gmail.com>
2024-05-12 02:39:42 -0700

    home screen testing

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -7,12 +7,17 @@ import { StyleSheet, Text, View } from 'react-native';
 import { useEffect, useState } from 'react';
 import DocumentScanner from 'react-native-document-scanner-plugin';
 import { DemoButton } from '../components/ui/DemoButton';
-import * as routes from '../routes';
+import routes, { IHomeScreenDrawerProps } from '../routes';
 import { groupNames } from './GroupsScreen'; // Importing groups from defineGroups
-import { HomeScreenNavigationProps } from '../Navigator';
+import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
+import firebase from '../components/state/firebaseStorage';
+import useAppContext from '../components/hooks/useAppContext';
+import utility from '../components/util/utility';
 
-export default function HomeScreen(props: HomeScreenNavigationProps) {
+
+export default function HomeScreen(props: IHomeScreenDrawerProps) {
   
+  const ctx = useAppContext();
   const [scannedImage, setScannedImage] = useState<string | undefined>();
 
   const scanDocument = async () => {
@@ -41,17 +46,31 @@ export default function HomeScreen(props: HomeScreenNavigationProps) {
 
   return (
     <View style={styles.container}>
-      <View style={{height: 48}}>
+      <View style={{height: 60}}>
         <DemoButton key="Process Image" onPress={() => scanDocument()}>
           {'Process Image'}
         </DemoButton>
       </View>
-      <View style={{height: 48}}>
+      <View style={{height: 60, marginTop: 40}}>
         <DemoButton key="Choose Group" onPress={chooseGroup}>
           {'Choose Group'}
         </DemoButton>
       </View>
-      
+      <View style={{height: 60, marginTop: 40}}>
+        <DemoButton key="Test Create Doc" onPress={() => firebase.createUserAsync(({
+          ...utility.createEmptyUserObject(),
+          name: "David",
+          email: "dsserrat@ucsc.edu",
+          uid: ctx.authenticated?.uid ?? "123"
+        }))}>
+          {'Test Create Doc'}
+        </DemoButton>
+      </View>
+      <View style={{height: 60, marginTop: 40}}>
+        <DemoButton key="Test Get Doc" onPress={() => ctx.fetchUserCloudData()}>
+          {'Test Get Doc'}
+        </DemoButton>
+      </View>
     </View>
   );
 }

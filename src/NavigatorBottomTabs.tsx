9d0aca4511f6740cf101cb0387fd0593b73f31ed
David Serrato <davserrato@gmail.com>
2024-05-12 02:37:18 -0700

    updated navigation

---
+++ src/NavigatorBottomTabs.tsx
@@ -0,0 +1,43 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import useAppContext from './components/hooks/useAppContext';
+import routes, { IRootParamList } from './routes';
+import LoginScreen from './screens/LoginScreen';
+import SignUpScreen from './screens/SignUpScreen';
+import LogoutScreen from './screens/LogoutScreen';
+import HomeScreen from './screens/HomeScreen';
+import { SelectImageScreen } from './screens/SelectImageScreen';
+import GroupsScreen from './screens/GroupsScreen';
+import SettingsScreen from './screens/SettingsScreen';
+import { ProcessImageScreen } from './screens/ProcessImageScreen';
+
+
+const Tab = createBottomTabNavigator<IRootParamList>();
+
+export default function NavigatorBottomTabs() {
+
+  const ctx = useAppContext();
+
+  return (
+    <Tab.Navigator>
+      {ctx.authenticated == null ? (
+          <>
+            <Tab.Screen name={routes.LOGIN_SCREEN} component={LoginScreen} />
+            <Tab.Screen name={routes.SIGNUP_SCREEN} component={SignUpScreen} />
+            
+            <Tab.Screen name={routes.LOGOUT_SCREEN} component={LogoutScreen} />
+          </>
+        ) : 
+        (
+          <>
+            <Tab.Screen name={routes.HOME_SCREEN} component={HomeScreen} />
+            <Tab.Screen name={routes.SELECT_SCREEN} component={SelectImageScreen} />
+            <Tab.Screen name={routes.GROUPS_SCREEN} component={GroupsScreen} />
+            <Tab.Screen name={routes.SETTINGS_SCREEN} component={SettingsScreen} />
+            <Tab.Screen name={routes.LOGOUT_SCREEN} component={LogoutScreen} />
+
+            <Tab.Screen name={routes.PROCESS_IMAGE_SCREEN} component={ProcessImageScreen} />
+          </>
+        )}
+    </Tab.Navigator>
+  )
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,4 @@
+import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
 import { DrawerScreenProps } from "@react-navigation/drawer";
 import { RouteProp } from "@react-navigation/native";
 
@@ -6,12 +7,71 @@ import { RouteProp } from "@react-navigation/native";
  * 
  * Route names
  */
-export const HOME_SCREEN = "Home";
-export const SELECT_SCREEN = 'Select Image';
-export const PROCESS_IMAGE_SCREEN = 'Process Image';
-export const SETTINGS_SCREEN = "Settings";
-export const GROUPS_SCREEN = "Groups";
-export const CONTACTS_SCREEN = "Contacts";
-export const LOGIN_SCREEN = "Login";
-export const LOGOUT_SCREEN = "Logout";
-export const SIGNUP_SCREEN = "Sign Up";
+
+// Defines what parameters gets sent to a screen route
+export type IRootParamList = {
+  [routes.HOME_SCREEN]: undefined,
+  [routes.SELECT_SCREEN]: undefined,
+  [routes.PROCESS_IMAGE_SCREEN]: {uri: string;},
+  [routes.GROUPS_SCREEN]: undefined,
+  [routes.CONTACTS_SCREEN]: undefined,
+  [routes.SETTINGS_SCREEN]: undefined,
+  [routes.LOGIN_SCREEN]: undefined,
+  [routes.LOGOUT_SCREEN]: undefined,
+  [routes.SIGNUP_SCREEN]: undefined
+};
+
+// Drawer
+// Adds additional methods to the "prop" variables of each screen, such as navigate()
+export type IHomeScreenDrawerProps = DrawerScreenProps<IRootParamList, typeof routes.HOME_SCREEN>;
+export type ISelectScreenDrawerProps = DrawerScreenProps<IRootParamList, typeof routes.SELECT_SCREEN>;
+export type IProcessImageDrawerProps = DrawerScreenProps<IRootParamList, typeof routes.PROCESS_IMAGE_SCREEN>;
+export type IGroupsDrawerProps = DrawerScreenProps<IRootParamList, typeof routes.GROUPS_SCREEN>;
+export type IContactsScreenDrawerProps = DrawerScreenProps<IRootParamList, typeof routes.CONTACTS_SCREEN>;
+export type ISettingsScreenDrawerProps = DrawerScreenProps<IRootParamList, typeof routes.SETTINGS_SCREEN>;
+export type ILoginScreenDrawerProps = DrawerScreenProps<IRootParamList, typeof routes.LOGIN_SCREEN>;
+export type ILogoutScreenDrawerProps = DrawerScreenProps<IRootParamList, typeof routes.LOGOUT_SCREEN>;
+export type ISignUpScreenDrawerProps = DrawerScreenProps<IRootParamList, typeof routes.SIGNUP_SCREEN>;
+
+// Bottom Tabs
+// Adds additional methods to the "prop" variables of each screen, such as navigate()
+export type IHomeScreenTabProps = BottomTabScreenProps<IRootParamList, typeof routes.HOME_SCREEN>;
+export type ISelectScreenTabProps = BottomTabScreenProps<IRootParamList, typeof routes.SELECT_SCREEN>;
+export type IProcessImageTabProps = BottomTabScreenProps<IRootParamList, typeof routes.PROCESS_IMAGE_SCREEN>;
+export type IGroupsTabProps = BottomTabScreenProps<IRootParamList, typeof routes.GROUPS_SCREEN>;
+export type IContactsScreenTabProps = BottomTabScreenProps<IRootParamList, typeof routes.CONTACTS_SCREEN>;
+export type ISettingsScreenTabProps = BottomTabScreenProps<IRootParamList, typeof routes.SETTINGS_SCREEN>;
+export type ILoginScreenTabProps = BottomTabScreenProps<IRootParamList, typeof routes.LOGIN_SCREEN>;
+export type ILogoutScreenTabProps = BottomTabScreenProps<IRootParamList, typeof routes.LOGOUT_SCREEN>;
+export type ISignUpScreenTabProps = BottomTabScreenProps<IRootParamList, typeof routes.SIGNUP_SCREEN>;
+
+// Adds additional methods to the prop type. This one is combined with another type on the Process Image screen
+export type IProcessImageRouteProps = RouteProp<IRootParamList, typeof routes.PROCESS_IMAGE_SCREEN>;
+
+
+// Routes
+export interface IRoutes {
+  HOME_SCREEN: "Home",
+  SELECT_SCREEN: "Select Image",
+  PROCESS_IMAGE_SCREEN: "Process Image",
+  SETTINGS_SCREEN: "Settings",
+  GROUPS_SCREEN: "Groups",
+  CONTACTS_SCREEN: "Contacts",
+  LOGIN_SCREEN: "Login",
+  LOGOUT_SCREEN: "Logout",
+  SIGNUP_SCREEN: "Sign Up",
+}
+
+const routes: IRoutes = {
+  HOME_SCREEN: "Home",
+  SELECT_SCREEN: "Select Image",
+  PROCESS_IMAGE_SCREEN: "Process Image",
+  SETTINGS_SCREEN: "Settings",
+  GROUPS_SCREEN: "Groups",
+  CONTACTS_SCREEN: "Contacts",
+  LOGIN_SCREEN: "Login",
+  LOGOUT_SCREEN: "Logout",
+  SIGNUP_SCREEN: "Sign Up"
+}
+
+export default routes;

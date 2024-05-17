f7e8f7121abf98a7db14387cb69f79bb2b510acb
ToeMash <thomas2001.stearns@gmail.com>
2024-05-16 18:34:08 -0700

    fixed imports on ctests

--- src/parsers/ctests.ts
+++ src/parsers/ctests.ts
@@ -1,13 +1,5 @@
 import { ITextRecognitionResponse } from "../components/mlkit";
-import * as parseFunctions from "./parser";
-import React, {useCallback, useEffect, useState} from 'react';
-import {Image, useWindowDimensions, ScrollView} from 'react-native';
-import {ProcessImageNavigationProps, ProcessImageRouteProps} from '../Navigator';
-import { ViewOverlay } from '../components/ViewOverlay';
-import { TabView, SceneMap } from 'react-native-tab-view';
-import { ViewDictionary } from '../components/ViewDictionary';
-import { ViewReceipt } from '../components/ViewReceipt';
-import { ViewResponse } from '../components/ViewResponse';
+import parseFunctions from "./parserTools";
 import parser from '../parsers/parser';
 
 function buildValidDict(num: number): {[key: string]: number} {
@@ -62,32 +54,3 @@ export function testChecksum(num: number): number {
     console.log("testChecksum passed " + test_passed + " tests of " + num_tests*2)
     return test_passed
 };
-
-const processImage = async (url: string) => {
-    if (url) {
-      try {
-        // Send a request to Google's ML Kit
-        const response = await recognizeImage(url);
-
-        // If the response contains data
-        if (response?.blocks?.length > 0) {
-
-          // Process response here
-          setResponse(response);                            // Save the response
-          setAspectRatio(response.height / response.width); // Set the aspect ratio of the returned data 
-
-          // TO DO: What else do we want to do with the ML Kit response?
-          // console.log(getStore(response))
-          // console.log(isPrice("4.43"))
-
-          let dict = parser.parseOutput(response)
-          console.log(dict)
-          if (dict != undefined) {
-            console.log(parser.checksum(dict))
-          }
-        }
-    } catch (error) {
-        console.error(error);
-      }
-    }
-  };
